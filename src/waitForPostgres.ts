b372fa5b95385c3cb0226090a70c3d5ee7bce31e
Jam Risser <jam@codejam.ninja>
2020-09-15 07:28:19 +0000

    Fixed wait for postgres

--- src/waitForPostgres.ts
+++ src/waitForPostgres.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import execa from 'execa';
+import execa, { ExecaError } from 'execa';
 import ora from 'ora';
 import path from 'path';
 
@@ -17,14 +17,19 @@ const spinner = ora();
 async function waitForPostgres(interval = 1000) {
   if (!env.POSTGRES_URL) throw new Error('$POSTGRES_URL not set');
   for (;;) {
-    if (
-      (
-        await execa('psql', [env.POSTGRES_URL, '-c', '\\l'], {
-          stdio: 'pipe'
-        })
-      ).exitCode === 0
-    ) {
-      break;
+    try {
+      if (
+        (
+          await execa('psql', [env.POSTGRES_URL, '-c', '\\l'], {
+            stdio: 'pipe'
+          })
+        ).exitCode === 0
+      ) {
+        break;
+      }
+    } catch (err) {
+      const execaErr: ExecaError = err;
+      if (typeof execaErr.exitCode !== 'number') throw err;
     }
     await new Promise((r) => setTimeout(r, interval));
   }
