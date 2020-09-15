4b35604c2b324bcf6cf88b8a6b5405df64f1ffe6
Jam Risser <jam@codejam.ninja>
2020-09-15 07:22:02 +0000

    Updated wait for postgres script

--- scripts/wait-for-postgres.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-until psql "$POSTGRES_URL" -c '\l' >/dev/null; do
-  sleep 1
-done
--- src/waitForPostgres.ts
+++ src/waitForPostgres.ts
@@ -3,17 +3,29 @@ import execa from 'execa';
 import ora from 'ora';
 import path from 'path';
 
-const { argv } = process;
+const { argv, env } = process;
 const prismaPath = path.resolve(process.cwd(), argv[2] || 'prisma');
 dotenv.config({ path: path.resolve(prismaPath, '.env') });
 const spinner = ora();
 
 (async () => {
   spinner.start('waiting for postgres');
-  const waitForPostgres = path.resolve(
-    __dirname,
-    '../scripts/wait-for-postgres.sh'
-  );
-  await execa('sh', ['-c', waitForPostgres], { stdio: 'inherit' });
+  await waitForPostgres();
   spinner.succeed('postgres ready');
 })();
+
+async function waitForPostgres(interval = 1000) {
+  if (!env.POSTGRES_URL) throw new Error('$POSTGRES_URL not set');
+  for (;;) {
+    if (
+      (
+        await execa('psql', [env.POSTGRES_URL, '-c', '\\l'], {
+          stdio: 'pipe'
+        })
+      ).exitCode === 0
+    ) {
+      break;
+    }
+    await new Promise((r) => setTimeout(r, interval));
+  }
+}
