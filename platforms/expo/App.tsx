fc94f735c4c8d1e71e5e8ec027ce1d48ba4ab866
Clay Risser <clayrisser@gmail.com>
2023-01-22 08:53:24 -0600

    Started keycloak integration

--- platforms/expo/App.tsx
+++ platforms/expo/App.tsx
@@ -2,13 +2,11 @@ import "expo-dev-client";
 import React from "react";
 import { GlobalProvider } from "app/providers";
 import { NativeNavigation } from "app/navigation/native";
+import { fonts } from "app/fonts";
 import { useFonts } from "expo-font";
 
 export default function App() {
-  const [loaded] = useFonts({
-    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
-    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
-  });
+  const [loaded] = useFonts(fonts);
   if (!loaded) return null;
 
   return (
