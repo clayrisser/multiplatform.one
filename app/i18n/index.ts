26e49002902c90ae987d6fd9ca7c933e061c56b6
Clay Risser <clayrisser@gmail.com>
2023-01-28 13:58:49 -0600

    Updated packages

--- app/i18n/index.ts
+++ app/i18n/index.ts
@@ -2,11 +2,14 @@ import 'intl-pluralrules';
 import i18n, { Resource, ResourceKey } from 'i18next';
 import locales from './locales';
 import { MultiPlatform } from 'multiplatform.one';
-import { config } from 'app/config';
 import { defaultNamespace, defaultLocale, supportedLocales } from './config';
 import { initReactI18next } from 'react-i18next';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+
+const logger = console;
+
+if (MultiPlatform.isNext && !MultiPlatform.isStatic) {
+  logger.warn("next should not import 'app/i18n'");
+}
 
 i18n.use(initReactI18next).init({
   defaultNS: defaultNamespace,
@@ -19,38 +22,6 @@ i18n.use(initReactI18next).init({
     escapeValue: false,
   },
 });
-
-export function useLocale(): [string, (locale: string) => void] {
-  const nextRouter = MultiPlatform.isNext() && config.get('NEXT_STATIC') !== '1' ? useRouter() : null;
-  const [locale, setLocale] = useState(nextRouter?.locale || i18n?.language || 'en');
-
-  useEffect(() => {
-    if (!nextRouter?.locale) return;
-    setLocale(nextRouter.locale);
-  }, [nextRouter?.locale]);
-
-  useEffect(() => {
-    function handleLanguageChanged(lng: string) {
-      setLocale(lng);
-    }
-    i18n.on('languageChanged', handleLanguageChanged);
-    return () => {
-      i18n.off('languageChanged', handleLanguageChanged);
-    };
-  }, []);
-
-  let changeLocale = (locale: string) => {
-    i18n?.changeLanguage(locale);
-  };
-  if (nextRouter) {
-    changeLocale = (locale: string) => {
-      const { pathname, asPath, query } = nextRouter;
-      nextRouter.push({ pathname, query }, asPath, { locale });
-    };
-  }
-
-  return [locale, changeLocale];
-}
 
 export { supportedLocales, defaultLocale, defaultNamespace };
 
