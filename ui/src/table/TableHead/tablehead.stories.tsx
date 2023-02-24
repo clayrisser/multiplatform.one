9f22cafe56014f747be16246a9499ba74b9072e6
Lalit Rajak <mecha.lalit@gmail.com>
2023-02-24 09:41:47 +0530

    fixing  the merge conflict

--- ui/src/table/TableHead/tablehead.stories.tsx
+++ ui/src/table/TableHead/tablehead.stories.tsx
@@ -10,7 +10,7 @@ export const main = () => (
   <TableHead
     label={['a', 'b', 'c', 'd', 'e', 'f']}
     backgroundColor="black"
-    textStyle={{ col: 'blue' }}
+    headTextStyle={{ col: 'blue' }}
     jc="space-between"
   />
 );
