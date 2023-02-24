039c0b234da061375be01cdcc479c22ba9abc0c0
Ajith Kumar <ajithkrm6@gmail.com>
2023-02-24 15:17:55 +0530

    created a basic Table component

---
+++ ui/src/.Table/Column.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { YStack, YStackProps, Text, TextProps, XStack } from 'tamagui';
+
+export interface ColumnDataProps {
+  data: any[];
+  height?: number;
+  width?: number;
+}
+
+export type ColumnProps = { textProps?: TextProps } & { stackProps?: YStackProps } & ColumnDataProps;
+
+export const Column = ({ stackProps, textProps, ...props }: ColumnProps) => {
+  return (
+    <YStack>
+      {props.data.map((val, index) => {
+        return (
+          <XStack {...stackProps} key={index}>
+            {val.map((eachVal, i) => (
+              <Text {...textProps} key={i}>
+                {eachVal}
+              </Text>
+            ))}
+          </XStack>
+        );
+      })}
+    </YStack>
+  );
+};
