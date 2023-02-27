d0dabf9d85784a6c3555d669d4189786a3a4883f
dharmendra <dharmendra.developer6@gmil.com>
2023-02-27 16:59:47 +0530

    addigng args to table component screens

--- ui/src/screens/tableComponent.stories.tsx
+++ ui/src/screens/tableComponent.stories.tsx
@@ -5,23 +5,40 @@ export default {
   title: 'Screens/TableComponent',
 };
 
-export const main = () => {
+export const main = (args: any) => {
   return (
-    <Table yStack={{}}>
-      <Header xStack={{}} text={{ padding: 10 }} columns={['S.no', 'Name', 'Age', 'Location']} />
-      <Rows
-        yStack={{}}
-        xStack={{}}
-        text={{}}
-        rows={[
-          [1, 'Ben', 28, 'Texas'],
-          [2, 'Tom', 30, 'Texas'],
-          [3, 'Dharmendra Boddeda', 25, 'Texas'],
-          [4, 'Ajith', 27, 'Vizag'],
-          [5, 'Rani', 23, 'Vizag'],
-        ]}
-      />
-      {/* <Row xStack={{}} text={{}} row={[5, 'Rani', 23, 'Vizag']} /> */}
+    <Table {...args.table}>
+      <Header {...args.columns} />
+      <Rows {...args.rows} />
+      {/* <Row {...args.row} xStack={{}} text={{}} row={[5, 'Bob', 23, 'rob']} /> */}
     </Table>
   );
+};
+
+main.args = {
+  table: {
+    yStack: {},
+  },
+  columns: {
+    xStack: {},
+    text: { padding: 10 },
+    columns: ['S.no', 'Name', 'Age', 'Location'],
+  },
+  rows: {
+    yStack: {},
+    xStack: {},
+    text: {},
+    rows: [
+      [1, 'Ben', 28, 'Texas'],
+      [2, 'Tom', 30, 'Texas'],
+      [3, 'Dharmendra Boddeda', 25, 'Texas'],
+      [4, 'Ajith', 27, 'Vizag'],
+      [5, 'Rani', 23, 'Vizag'],
+    ],
+  },
+  row: {
+    xStack: {},
+    text: {},
+    row: [],
+  },
 };
