e0692459fb7981637a5ec7995f33c54e1508c289
lavanya <katarilavanya881@gmail.com>
2023-02-28 18:31:46 +0530

    added code component from tamagui

---
+++ ui/src/CodyComponent/Cody/Cody.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { DocCodeBlock } from './index';
+
+export default {
+  title: 'ui/Cody',
+  component: DocCodeBlock,
+  parameters: { status: { type: 'beta' } },
+};
+
+export const main = () => <DocCodeBlock />;
