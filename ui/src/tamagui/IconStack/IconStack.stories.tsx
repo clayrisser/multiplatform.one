43fe9bb9e0fab374c02877c392f587f46b85c00e
Ajith Kumar <ajithkrm6@gmail.com>
2023-03-01 18:43:40 +0530

    added few more components

---
+++ ui/src/tamagui/IconStack/IconStack.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { IconStack } from './index';
+
+export default {
+  title: 'ui/tamagui/IconStack',
+  component: IconStack,
+  parameters: { status: { type: 'beta' } },
+};
+
+export const main = () => <IconStack>hello</IconStack>;
--- ui/src/tamagui/Link/Link.stories.tsx
+++ ui/src/tamagui/Link/Link.stories.tsx
@@ -1,2 +1,10 @@
 import React from 'react';
 import { Link } from './index';
+
+export default {
+  title: 'ui/tamagui/Link',
+  component: Link,
+  parameters: { status: { type: 'beta' } },
+};
+
+export const link = () => <Link href="https://www.tamagui.com">Tamagui</Link>;
