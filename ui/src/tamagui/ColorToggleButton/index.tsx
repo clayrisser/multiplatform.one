33724888da82410bcc6159216452648579c2a6f5
Manikanta <vrmk1995@gmail.com>
2023-03-01 12:25:29 +0530

    creating components in ui

---
+++ ui/src/tamagui/ColorToggleButton/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useTint } from 'ui/src/hooks';
+import type { ButtonProps } from 'tamagui';
+import { Button, Circle, TooltipSimple } from 'tamagui';
+
+export const ColorToggleButton = (props: ButtonProps) => {
+  const { tint, setNextTint } = useTint();
+  return (
+    <TooltipSimple groupId="header-actions-color" label="Next theme">
+      <Button size="$3" onPress={setNextTint} {...props} aria-label="Next theme">
+        <Circle bw={1} boc="var(--color9)" m={2} size={12} backgroundColor={tint} />
+      </Button>
+    </TooltipSimple>
+  );
+};
---
+++ ui/src/tamagui/DocsRouteNavItem/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { NextLink } from 'ui/src/NextLink';
+import { SizableText, Spacer, XStack, YStack } from 'tamagui';
+import type { NavItemProps } from 'ui/src/DocsPage';
+import { ExternalIcon } from 'ui/src/ExternalIcon';
+
+export const DocsRouteNavItem = React.memo(({ children, active, href, pending, ...props }: NavItemProps) => {
+  const isExternal = href.startsWith('http');
+  return (
+    <NextLink href={href}>
+      <XStack
+        className="docs-nav-item all ease-in ms150"
+        {...props}
+        {...(isExternal ? { target: '_blank', rel: 'noopener noreferrer' } : {})}
+        ai="center"
+        jc="flex-end"
+        py="$0.25"
+        px="$4"
+        opacity={pending ? 0.25 : 1}
+        pressStyle={{
+          backgroundColor: '$background',
+        }}
+        pointerEvents={pending ? 'none' : 'auto'}
+        pos="relative"
+        $sm={{
+          py: '$1',
+        }}
+      >
+        <YStack
+          className="sidebar-indicator"
+          o={active ? 1 : 0}
+          pos="absolute"
+          t={0}
+          b={0}
+          r={0}
+          w={1}
+          bc={active ? '$color' : '$backgroundHover'}
+        />
+        <SizableText
+          className="name"
+          size="$4"
+          userSelect="none"
+          opacity={0.65}
+          theme={active ? null : 'alt1'}
+          {...(active && {
+            fow: '900',
+            opacity: 1,
+          })}
+        >
+          {children}
+        </SizableText>
+        {isExternal && (
+          <XStack opacity={0.5}>
+            <Spacer size="$2" />
+            <ExternalIcon />
+          </XStack>
+        )}
+        {pending ? (
+          <>
+            <XStack flex={1} />
+            <SizableText theme="alt2" size="$1" px="$2" py="$1" bc="$background" borderRadius="$3">
+              WIP
+            </SizableText>
+          </>
+        ) : null}
+      </XStack>
+    </NextLink>
+  );
+});
