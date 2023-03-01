867fdbbad3eebcf68b6eb8d1ff7bb6a8cbd476d1
lavanya <katarilavanya881@gmail.com>
2023-03-01 17:21:59 +0530

    added ui components

---
+++ ui/src/tamagui/TamaguiExamplesCode/TamaguiExampleCode.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { TamaguiExamples } from './index';
+
+export default {
+  title: 'ui/tamagui/TamaguiExamples',
+  component: TamaguiExamples,
+  parameters: { status: { type: 'beta' } },
+};
+export const main = () => <TamaguiExamples />;
---
+++ ui/src/tamagui/TintSection/TintSection.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { TintSection } from './index';
+
+export default {
+  title: 'ui/tamagui/TintSection',
+  component: TintSection,
+  parameters: { status: { type: 'beta' } },
+};
+export const main = () => <TintSection index={0} />;
