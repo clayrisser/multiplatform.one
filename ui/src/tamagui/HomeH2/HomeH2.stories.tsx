127728eeb6d0702d2b3c576ee5a81fc778557cac
pravallika kunduru <Pravallikareddy2708@gmail.com>
2023-03-01 17:05:18 +0530

    created Homeh2 component

---
+++ ui/src/tamagui/HomeH2/HomeH2.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HomeH2 } from './index';
+
+export default {
+  title: 'ui/tamagui/HomeH2',
+  component: HomeH2,
+  parameters: { status: { type: 'beta' } },
+};
+
+export const main = () => <HomeH2 />;
