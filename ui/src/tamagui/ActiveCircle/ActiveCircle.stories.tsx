40471717b17c6322c08573e31533ed9cdd604741
pravallika kunduru <Pravallikareddy2708@gmail.com>
2023-03-01 18:28:36 +0530

    created few  components

---
+++ ui/src/tamagui/ActiveCircle/ActiveCircle.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ActiveCircle } from './index';
+
+export default {
+  title: 'ui/tamagui/ActiveCircle',
+  component: ActiveCircle,
+  parameters: { status: { type: 'beta' } },
+};
+
+export const main = () => <ActiveCircle />;
---
+++ ui/src/tamagui/Card/Card.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from './index';
+
+export default {
+  title: 'ui/tamagui/Card',
+  component: Card,
+  parameters: { status: { type: 'beta' } },
+};
+
+export const main = () => <Card />;
