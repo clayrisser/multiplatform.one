f54a1339dccab443fbeb7f11b1af6f2261e367dd
Lalit Rajak <tech.mechanicallalit@gmail.com>
2023-07-25 17:57:33 +0530

    working on creating an custom input box

---
+++ packages/components/src/component/SimpleInput/SimpleInput.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { SimpleInput } from '.';
+import { YStack } from 'tamagui';
+import { Key } from '@tamagui/lucide-icons';
+
+export default {
+  title: 'components/SimpleInput',
+  component: SimpleInput,
+  parameters: { status: { type: 'beta' } },
+};
+
+export const password = () => (
+  <YStack justifyContent="center" alignItems="center">
+    <SimpleInput password size="$3" iconAfter={Key} />
+  </YStack>
+);
+
+export const iconBefore = () => <SimpleInput password width={800} height={50} iconBefore={Key} />;
+
+export const iconAfter = () => (
+  <SimpleInput width={250} height={50} iconAfter={Key} placeholder="icon after input box" />
+);
+
+export const circular = () => <SimpleInput placeholder="icon after input box" circular />;
---
+++ packages/components/src/component/SimpleInput/index.tsx
@@ -0,0 +1,151 @@
+import type { ButtonProps, ColorTokens, InputProps, SizeTokens, ThemeTokens, XStackProps } from 'tamagui';
+import { Button, Input, XGroup, XStack } from 'tamagui';
+import { Eye, EyeOff } from '@tamagui/lucide-icons';
+import React from 'react';
+import type { SvgProps } from 'react-native-svg';
+
+export interface BaseIconProps {
+  size?: number | SizeTokens;
+  strokeWidth?: number | SizeTokens;
+  color?: (ColorTokens | ThemeTokens | (string & {})) | null;
+  disableTheme?: boolean;
+  style?: any;
+}
+
+interface CustomProps {
+  password?: boolean;
+  iconBefore?: React.NamedExoticComponent<IconProps>;
+  iconAfter?: React.NamedExoticComponent<IconProps>;
+  iconStyle?: ButtonProps;
+  circular?: boolean;
+  iconBeforeScale?: number;
+  iconAfterScale?: number;
+  iconPasswordScale?: number;
+  iconsAsButton?: boolean;
+  iconBeforeAsButton?: boolean;
+  iconAfterAsButton?: boolean;
+}
+
+type IconProps = SvgProps & BaseIconProps;
+
+export type SimpleInputProps = InputProps & CustomProps & XStackProps;
+
+export function SimpleInput({
+  value,
+  size,
+  password,
+  iconBefore,
+  iconAfter,
+  height,
+  width,
+  iconStyle,
+  circular,
+  placeholder,
+  iconAfterScale,
+  iconBeforeScale,
+  iconPasswordScale,
+  iconAfterAsButton,
+  iconBeforeAsButton,
+  iconsAsButton,
+  onChangeText,
+  paddingHorizontal,
+  ...props
+}: SimpleInputProps) {
+  const [show, setShow] = React.useState(false);
+  const [inputValue, setInputValue] = React.useState<string | undefined>(value ?? '');
+  const displayValue = inputValue && password && !show ? '*'.repeat(inputValue.length) : inputValue;
+  const [borderWidth, setBorderWidth] = React.useState(0);
+  const totalIcons = (iconBefore ? 1 : 0) + (iconAfter || password ? 1 : 0);
+  const inputValuePadding = iconBefore ? 0 : 24;
+  const iconPercentage = totalIcons === 1 ? '20%' : totalIcons === 2 ? '10%' : undefined;
+  const inputPercentage = iconPercentage ? '80%' : '100%';
+
+  React.useEffect(() => {
+    if (!inputValue) return;
+    if (onChangeText) onChangeText(inputValue);
+  }, [inputValue]);
+
+  function handleValueChange(text: string) {
+    setInputValue(text);
+    if (onChangeText) onChangeText(text);
+  }
+
+  return (
+    <XStack>
+      <XGroup
+        {...props}
+        height={size}
+        borderRadius={circular ? 9999999 : props.borderRadius || size}
+        borderWidth={borderWidth}
+        borderColor="$backgroundFocus"
+        minHeight={height || '$5'}
+        width={width || '$20'}
+      >
+        {iconBefore && (
+          <Button
+            icon={iconBefore}
+            unstyled={!(iconsAsButton || iconBeforeAsButton)}
+            {...iconStyle}
+            jc="center"
+            ai="center"
+            scaleIcon={iconBeforeScale}
+            width={iconPercentage}
+            height="100%"
+            padding={0}
+          />
+        )}
+        <Input
+          {...props}
+          size={size}
+          onChangeText={handleValueChange}
+          onFocus={(e) => {
+            setBorderWidth(2);
+            props.onFocus?.(e);
+          }}
+          onBlur={(e) => {
+            setBorderWidth(0);
+            props.onBlur?.(e);
+          }}
+          height="100%"
+          placeholder={placeholder}
+          width={inputPercentage}
+          paddingLeft={inputValuePadding}
+          paddingRight={inputValuePadding}
+          unstyled
+          value={displayValue}
+          borderWidth={0}
+          paddingHorizontal={paddingHorizontal}
+        />
+        {!!password && (
+          <Button
+            icon={show ? Eye : EyeOff}
+            scaleIcon={iconPasswordScale}
+            height="100%"
+            {...iconStyle}
+            width={iconPercentage}
+            onPress={(e) => {
+              setShow((prev) => !prev);
+              props.onPress?.(e);
+            }}
+            jc="center"
+            ai="center"
+            padding={0}
+          />
+        )}
+        {!!iconAfter && !password && (
+          <Button
+            icon={iconAfter}
+            {...iconStyle}
+            jc="center"
+            ai="center"
+            scaleIcon={iconBeforeScale}
+            width={iconPercentage}
+            unstyled={!(iconsAsButton || iconAfterAsButton)}
+            height="100%"
+            padding={0}
+          />
+        )}
+      </XGroup>
+    </XStack>
+  );
+}
