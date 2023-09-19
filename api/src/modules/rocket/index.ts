fd5cfbe96e131d5846b82fc995d862f95ca1d137
Clay Risser <clayrisser@gmail.com>
2023-09-19 01:12:12 -0500

    Added api

---
+++ api/src/modules/rocket/index.ts
@@ -0,0 +1,30 @@
+/*
+ *  File: /src/modules/rocket/index.ts
+ *  Project: api
+ *  File Created: 18-09-2023 08:18:09
+ *  Author: Clay Risser
+ *  -----
+ *  BitSpur (c) Copyright 2021 - 2023
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { RocketController } from './rocket.controller';
+
+@Module({
+  controllers: [RocketController],
+  imports: [HttpModule.register({})],
+})
+export class RocketModule {}
---
+++ api/src/modules/user/user.resolver.spec.ts
@@ -0,0 +1,40 @@
+/*
+ *  File: /src/modules/user/user.resolver.spec.ts
+ *  Project: api
+ *  File Created: 18-09-2023 08:18:09
+ *  Author: Clay Risser
+ *  -----
+ *  BitSpur (c) Copyright 2021 - 2023
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import type { TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { UserResolver } from './user.resolver';
+
+describe('UserResolver', () => {
+  let resolver: UserResolver;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserResolver],
+    }).compile();
+
+    resolver = module.get<UserResolver>(UserResolver);
+  });
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+});
