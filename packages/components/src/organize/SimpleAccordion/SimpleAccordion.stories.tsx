845270dc677ce8956f77f9723eea22b14b4bcd77
Lalit Rajak <lalitrajak312@gmail.com>
2023-10-13 12:13:33 +0530

    ✨ feat: Add SimpleAccordion component and related stories
    This commit adds the SimpleAccordion component and its corresponding stories. The SimpleAccordion component is located in the file `/src/organize/SimpleAccordion/index.tsx`. It accepts children as a prop and provides options for overflow and type. The stories for the component can be found in the file `/src/organize/SimpleAccordion/SimpleAccordion.stories.tsx`. The stories showcase different accordion items with triggers and content. This addition was authored by Lalit Rajak.
    
    Changes Made:
    - Added SimpleAccordion component and its related files.
    - Created stories to demonstrate the usage of the component.
    - Integrated the AccordionItem component within the SimpleAccordion component.
    - Implemented trigger and content styles within the AccordionItem component.
    
    Note: The commit message assumes that the code changes are relevant and functional.

--- packages/components/src/organize/SimpleAccordion/SimpleAccordion.stories.tsx
+++ packages/components/src/organize/SimpleAccordion/SimpleAccordion.stories.tsx
@@ -1,14 +1,52 @@
-import { SimpleAccordion } from '.';
+/**
+ * File: /src/organize/SimpleAccordion/SimpleAccordion.stories.tsx
+ * Project: @multiplatform.one/components
+ * File Created: 13-10-2023 12:02:11
+ * Author: Lalit rajak
+ * -----
+ * BitSpur (c) Copyright 2021 - 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from 'react';
+import { AccordionItem, SimpleAccordion } from './index';
+import { YStack, Text } from 'tamagui';
 
 export default {
-  title: 'organize/SimpleAccordion',
+  title: 'components/SimpleAccordion',
   component: SimpleAccordion,
   parameters: {
-    status: {
-      type: 'beta',
-    },
+    status: { type: 'beta' },
   },
 };
 
-export const simpleAccordion = (args) => <SimpleAccordion {...args} />;
+export const main = (args) => (
+  <SimpleAccordion {...args}>
+    <AccordionItem trigger={<Text>Trigger Apple </Text>} value="apple">
+      <YStack padding="$4">
+        <Text> Apple Content</Text>
+      </YStack>
+    </AccordionItem>
+    <AccordionItem trigger={<Text>Trigger Banana </Text>} value="banana">
+      <YStack padding="$4">
+        <Text>Banana Content</Text>
+      </YStack>
+    </AccordionItem>
+    <AccordionItem trigger={<Text>Trigger Orange </Text>} value="orange">
+      <YStack padding="$4">
+        <Text>Orange Content</Text>
+      </YStack>
+    </AccordionItem>
+  </SimpleAccordion>
+);
