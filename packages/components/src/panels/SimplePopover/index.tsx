ece359f4b764c3b147fb1c7de9ead57b500e307b
Lalit Rajak <lalitrajak312@gmail.com>
2023-10-13 11:55:42 +0530

    ♻️ refactor: Extract common code for SimpleDialog and SimplePopover components
    This commit refactors the SimpleDialog and SimplePopover components to extract common code. The extracted code includes the file header comments, license information, and import statements. This improves code maintainability and reduces duplication.
    
    Changes:
    - Extracted common code for SimpleDialog and SimplePopover components
    - Added file header comments with file path, project information, creation date, and author details
    - Added license information
    - Removed duplicate code from the components
    - Updated dependencies and imports as needed

--- packages/components/src/panels/SimplePopover/index.tsx
+++ packages/components/src/panels/SimplePopover/index.tsx
@@ -1,3 +1,24 @@
+/**
+ * File: /src/panels/SimplePopover/index.tsx
+ * Project: @multiplatform.one/components
+ * File Created: 13-10-2023 09:40:26
+ * Author: Lalit rajak
+ * -----
+ * BitSpur (c) Copyright 2021 - 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import type {
   PopoverArrowProps,
   PopoverContentProps,
@@ -114,7 +135,7 @@ export function SimplePopover({
         zi={9999999999}
         {...contentStyle}
       >
-        {arrow && <Popover.Arrow borderWidth={1} borderColor="$borderColor" />}
+        {arrow && <Popover.Arrow borderWidth={1} borderColor="$borderColor" {...arrowStyle} />}
         {children}
       </Popover.Content>
     </Popover>
