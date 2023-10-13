f58b8e3e56f8cb6c783de9d7048330b8b9bc6a57
Lalit Rajak <lalitrajak312@gmail.com>
2023-10-13 12:13:33 +0530

    ✨ feat: Add SimpleAccordion component and related stories
    This commit adds the SimpleAccordion component and its corresponding stories. The SimpleAccordion component is located in the file `/src/organize/SimpleAccordion/index.tsx`. It accepts children as a prop and provides options for overflow and type. The stories for the component can be found in the file `/src/organize/SimpleAccordion/SimpleAccordion.stories.tsx`. The stories showcase different accordion items with triggers and content. This addition was authored by Lalit Rajak.
    
    Changes Made:
    - Added SimpleAccordion component and its related files.
    - Created stories to demonstrate the usage of the component.
    - Integrated the AccordionItem component within the SimpleAccordion component.
    - Implemented trigger and content styles within the AccordionItem component.
    
    Note: The commit message assumes that the code changes are relevant and functional.

--- packages/components/src/organize/SimpleAccordion/index.tsx
+++ packages/components/src/organize/SimpleAccordion/index.tsx
@@ -1,6 +1,35 @@
-import { Text } from 'tamagui';
+import type { AccordionContentProps, AccordionItemProps, AccordionTriggerProps } from 'ui';
+import { Accordion } from 'ui';
 import React from 'react';
 
-export function SimpleAccordion() {
-  return <Text>SimpleAccordion</Text>;
+interface SimpleAccordionProps {
+  children: React.ReactNode;
+  overflow?: 'visible' | 'hidden' | 'scroll';
+  type?: 'single' | 'multiple';
+}
+
+type SimpleAccordionItemProps = AccordionItemProps & {
+  trigger: React.ReactNode;
+  triggerStyle?: AccordionTriggerProps;
+  contentStyle?: AccordionContentProps;
+  children: React.ReactNode;
+};
+
+export function SimpleAccordion({ children, ...props }: SimpleAccordionProps) {
+  return (
+    <Accordion overflow="hidden" type="single" {...props}>
+      {children}
+    </Accordion>
+  );
+}
+
+export function AccordionItem({ trigger, triggerStyle, children, contentStyle, ...props }: SimpleAccordionItemProps) {
+  return (
+    <Accordion.Item {...props}>
+      <Accordion.Trigger flexDirection="row" justifyContent="space-between" {...triggerStyle}>
+        {trigger}
+      </Accordion.Trigger>
+      <Accordion.Content {...contentStyle}>{children}</Accordion.Content>
+    </Accordion.Item>
+  );
 }
