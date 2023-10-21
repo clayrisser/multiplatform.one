58ad2a72b75d8cf4afbb6575b5b2878dfdf51cac
Lalit Rajak <lalitrajak312@gmail.com>
2023-10-21 11:40:20 +0530

    ♻️ refactor: rename Carousel component and related files to SimpleCarousel
    The Carousel component and its associated files have been renamed to SimpleCarousel to provide a more accurate and descriptive name. This change improves code readability and maintainability.

--- packages/components/src/organize/index.ts
+++ packages/components/src/organize/index.ts
@@ -18,7 +18,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-export * from './Carousel';
+export * from './SimpleCarousel';
 export * from './SimpleAccordion';
 export * from './SimpleInput';
 export * from './SimpleTabs';
