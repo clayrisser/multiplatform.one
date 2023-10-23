79d28de290af01efa93be68a3b0f961a4f165331
Lalit Rajak <lalitrajak312@gmail.com>
2023-10-23 12:49:38 +0530

    feat(components): add FormSwitch component
    - Added `FormSwitch` component to the components library.
    - Updated `FormSwitch.stories.tsx` file to use the new component.
    - Created a simple form with the `FormSwitch` component and a submit button.
    
    File Changes:
    - Modified `packages/components/src/forms/FormSwitch/FormSwitch.stories.tsx`
    - Modified `packages/components/src/forms/FormSwitch/index.tsx`

--- packages/components/src/forms/FormSwitch/index.tsx
+++ packages/components/src/forms/FormSwitch/index.tsx
@@ -1,3 +1,24 @@
+/**
+ * File: /src/forms/FormSwitch/index.tsx
+ * Project: @multiplatform.one/components
+ * File Created: 18-10-2023 15:23:17
+ * Author: Lalit rajak
+ * -----
+ * BitSpur (c) Copyright 2021 - 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, { useId } from 'react';
 import type { ComponentProps } from 'react';
 import type { FormControllerProps } from '../types';
@@ -57,12 +78,10 @@ export function FormSwitch({
             checked={value ?? false}
             id={id}
             {...switchProps}
-            onCheckedChange={(e) => {
-              onChange(e);
-              if (switchProps.onCheckedChange) switchProps.onCheckedChange(e);
-            }}
+            onCheckedChange={switchProps.onCheckedChange ?? onChange}
             onPress={(e) => {
               e.preventDefault();
+              onChange(e);
               if (switchProps.onPress) switchProps.onPress(e);
             }}
           >
