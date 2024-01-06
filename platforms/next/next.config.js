e26bd75379220aeccb7cee4178bb6ec3e698dfb1
Clay Risser <clayrisser@gmail.com>
2024-01-06 02:41:31 -0600

    Fixed nextjs platform

--- platforms/next/next.config.js
+++ platforms/next/next.config.js
@@ -1,3 +1,24 @@
+/*
+ *  File: /next.config.js
+ *  Project: @platform/next
+ *  File Created: 10-10-2023 06:39:34
+ *  Author: Clay Risser
+ *  -----
+ *  BitSpur (c) Copyright 2021 - 2024
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 process.env.IGNORE_TS_CONFIG_PATHS = 'true';
 process.env.TAMAGUI_TARGET = 'web';
 process.env.TAMAGUI_DISABLE_WARN_DYNAMIC_LOAD = '1';
@@ -66,7 +87,6 @@ module.exports = function (phase) {
     transpilePackages: transpileModules,
     experimental: {
       esmExternals: 'loose',
-      legacyBrowsers: false,
       optimizeCss: phase !== PHASE_DEVELOPMENT_SERVER,
       scrollRestoration: true,
     },
