e33828da9646617d68ee4ac3d7f7493488dd4f6b
Clay Risser <clayrisser@gmail.com>
2024-04-26 18:29:52 -0400

    Fixed package build

--- app/screens/user/index.tsx
+++ app/screens/user/index.tsx
@@ -1,9 +1,30 @@
+/**
+ * File: /screens/user/index.tsx
+ * Project: app
+ * File Created: 23-04-2024 05:52:22
+ * Author: Clay Risser
+ * -----
+ * BitSpur (c) Copyright 2021 - 2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from 'react';
 import { Button, Paragraph, YStack } from 'ui';
 import { ChevronLeft } from '@tamagui/lucide-icons';
 import { createParam } from 'solito';
 import { useLink } from 'solito/link';
-import { useTranslation } from 'multiplatform.one/locales';
+import { useTranslation } from '@multiplatform.one/locales';
 import { withDefaultLayout } from 'app/layouts/Default';
 
 const { useParam } = createParam<{ id: string }>();
--- packages/multiplatform.one/next/index.js
+++ packages/multiplatform.one/next/index.js
@@ -19,4 +19,4 @@
  *  limitations under the License.
  */
 
-module.exports = require('../lib/next/index.js');
+module.exports = require('../dist/cjs/next/index.js');
