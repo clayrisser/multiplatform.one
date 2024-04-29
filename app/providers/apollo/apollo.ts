4cfffef283d34802c76ba76ae257af02c9a53661
Clay Risser <clayrisser@gmail.com>
2024-04-29 19:53:40 -0400

    Separated apollo hook

---
+++ app/providers/apollo/apollo.ts
@@ -0,0 +1,59 @@
+/*
+ *  File: /providers/apollo/apollo.ts
+ *  Project: app
+ *  File Created: 29-04-2024 19:43:35
+ *  Author: Clay Risser
+ *  -----
+ *  BitSpur (c) Copyright 2021 - 2024
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { MultiPlatform } from 'multiplatform.one';
+import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { useKeycloak } from '@multiplatform.one/keycloak';
+import { useMemo } from 'react';
+
+const uri = 'http://localhost:5001/graphql';
+
+export function useApolloClient() {
+  const keycloak = useKeycloak();
+  return useMemo(() => {
+    const headers = {
+      ...(keycloak ? { Authorization: `Bearer ${keycloak?.token}` } : {}),
+    };
+    const httpLink = new HttpLink({ uri, headers });
+    const wsLink = new GraphQLWsLink(
+      createClient({
+        url: uri.replace(/^http/, 'ws'),
+        connectionParams: { headers },
+      }),
+    );
+    return new ApolloClient({
+      link: MultiPlatform.isServer
+        ? httpLink
+        : split(
+            ({ query }) => {
+              const definition = getMainDefinition(query);
+              return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+            },
+            wsLink,
+            httpLink,
+          ),
+      cache: new InMemoryCache(),
+    });
+  }, [keycloak?.token]);
+}
