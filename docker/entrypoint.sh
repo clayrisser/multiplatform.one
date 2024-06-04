9f7c20e4ef977aa181b2ed4bcc1d5128f9b6f22c
Clay Risser <clayrisser@gmail.com>
2024-06-04 07:45:52 -0400

    Updated packages

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,8 +13,11 @@ wait_for_postgres() {
     until psql "$1" -c '\q'; do sleep 1; done; sleep 1 && echo "postgres ready"
 }
 
+echo PRISMA_DATABASE_ENGINE $PRISMA_DATABASE_ENGINE
+
 if [ "$PRISMA_DATABASE_ENGINE" = "postgres" ]; then
     if [ "$POSTGRES_URL" = "" ]; then
+        echo HERE
         export POSTGRES_URL="postgresql://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_HOSTNAME}:${POSTGRES_PORT}/${POSTGRES_DATABASE}?sslmode=prefer"
         echo POSTGRES_URL="$POSTGRES_URL"
         wait_for_postgres "$POSTGRES_URL"
@@ -25,6 +28,8 @@ else
     echo "prisma database engine $PRISMA_DATABASE_ENGINE not supported"
     exit 1
 fi
+
+echo POSTGRES_URL $POSTGRES_URL
 
 mkdir -p /data
 (
