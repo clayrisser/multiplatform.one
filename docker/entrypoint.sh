7bce260dd73c5774f85ac375707560120bea5743
Clay Risser <clayrisser@gmail.com>
2024-06-04 08:30:56 -0400

    Fixed docker image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,23 +13,17 @@ wait_for_postgres() {
     until psql "$1" -c '\q'; do sleep 1; done; sleep 1 && echo "postgres ready"
 }
 
-echo PRISMA_DATABASE_ENGINE $PRISMA_DATABASE_ENGINE
-
 if [ "$PRISMA_DATABASE_ENGINE" = "postgres" ]; then
     if [ "$POSTGRES_URL" = "" ]; then
-        echo HERE
         export POSTGRES_URL="postgresql://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_HOSTNAME}:${POSTGRES_PORT}/${POSTGRES_DATABASE}?sslmode=prefer"
-        echo POSTGRES_URL="$POSTGRES_URL"
         wait_for_postgres "$POSTGRES_URL"
     fi
 elif [ "$PRISMA_DATABASE_ENGINE" = "sqlite" ]; then
     export SQLITE_URL="$DATA_DIR/sqlite.db"
 else
-    echo "prisma database engine $PRISMA_DATABASE_ENGINE not supported"
+    echo "prisma database engine $PRISMA_DATABASE_ENGINE not supported" >&2
     exit 1
 fi
-
-echo POSTGRES_URL $POSTGRES_URL
 
 mkdir -p /data
 (
