910953721a6c6a63705c64f814a6ecb157496acf
Clay Risser <clayrisser@gmail.com>
2024-06-06 13:58:36 -0400

    Started electron platform

---
+++ platforms/electron/vite.preload.config.ts
@@ -0,0 +1,44 @@
+/*
+ *  File: /vite.preload.config.ts
+ *  Project: @platform/electron
+ *  File Created: 06-06-2024 10:58:51
+ *  Author: Clay Risser
+ *  -----
+ *  BitSpur (c) Copyright 2021 - 2024
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import type { ConfigEnv, UserConfig } from 'vite';
+import { defineConfig, mergeConfig } from 'vite';
+import { getBuildConfig, external, pluginHotRestart } from './vite.base.config';
+
+export default defineConfig((env: ConfigEnv<'build'>) => {
+  const config: UserConfig = {
+    build: {
+      rollupOptions: {
+        external,
+        input: env.forgeConfigSelf.entry,
+        output: {
+          format: 'cjs',
+          inlineDynamicImports: true,
+          entryFileNames: '[name].js',
+          chunkFileNames: '[name].js',
+          assetFileNames: '[name].[ext]',
+        },
+      },
+    },
+    plugins: [pluginHotRestart('reload')],
+  };
+  return mergeConfig(getBuildConfig(env), config);
+});
