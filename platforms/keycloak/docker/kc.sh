89d6db0be9a3d14bdbf63661f35bd951e5544230
Clay Risser <clayrisser@gmail.com>
2024-06-09 16:40:41 -0400

    Updated keycloak Dockerfile

---
+++ platforms/keycloak/docker/kc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+IMPORT_FLAG_FILE=/opt/keycloak/imported.flag
+if [ "$KEYCLOAK_IMPORT" != "" ] && [ -f "$KEYCLOAK_IMPORT" ] && [ ! -f "$IMPORT_FLAG_FILE" ]; then
+    /opt/keycloak/bin/_kc.sh import --file "$KEYCLOAK_IMPORT"
+    touch "$IMPORT_FLAG_FILE"
+fi
+
+exec /opt/keycloak/bin/_kc.sh $@
