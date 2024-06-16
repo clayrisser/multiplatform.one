8172eedf9fb51776b87ef1febc995a3a98b6c6b2
Clay Risser <clayrisser@gmail.com>
2024-06-16 07:27:00 -0400

    Finished react native webpack

--- platforms/electron/nextron.config.js
+++ platforms/electron/nextron.config.js
@@ -22,166 +22,49 @@
 // https://github.com/expo/expo-cli/blob/master/packages/webpack-config/src/loaders/createBabelLoader.ts
 
 const path = require('path');
-const webpack = require('webpack');
-
-function getModule(name) {
-  return path.join('node_modules', name);
-}
-
-function getBabelPlugins(options) {
-  const reactNativeWeb = 'react-native-web';
-  if (options.babel.plugins && Array.isArray(options.babel.plugins)) {
-    return [reactNativeWeb, ...options.babel.plugins];
-  }
-  return [reactNativeWeb];
-}
-
-
-const defaultIncludes = [
-  getModule('@expo'),
-  getModule('@react'),
-  getModule('@unimodules'),
-  getModule('@use-expo'),
-  getModule('expo'),
-  getModule('native-base'),
-  getModule('react-native'),
-  getModule('react-navigation'),
-  getModule('styled-components'),
-  getModule('unimodules'),
-];
-
-const defaultExcludes = ['/node_modules', '/bower_components', '/.expo/', '(webpack)'];
-
-function isInstalled(name) {
-  try {
-    require(`${name}/package.json`);
-    return true;
-  } catch (err) {
-    return false;
-  }
-}
-
-
-
-function getRnPreset() {
-  if (isInstalled('@react-native/babel-preset')) {
-    return 'module:@react-native/babel-preset';
-  } else if (isInstalled('metro-react-native-babel-preset')) {
-    return 'module:metro-react-native-babel-preset';
-  }
-  throw new Error("couldn't find babel-preset-react-native or metro-react-native-babel-preset");
-}
-
-
-
-
-function webpackFinal(config, options) {
-  if (!config.babel) config.babel = {}
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.argv': 'process.argv',
-      'process.env': 'process.env',
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-      __DEV__: process.env.NODE_ENV !== 'production' || true,
-    }),
-  );
-  config.plugins.push(new webpack.DefinePlugin({ process: { env: {} } }));
-  const babelPlugins = getBabelPlugins(options);
-  const babelPresetReactNativeOptions = options?.babelPresetReactNativeOptions ?? {};
-  const babelPresetReactOptions = options?.babelPresetReactOptions ?? {};
-  const babelPresets = options?.babelPresets ?? [];
-  const babelExclude = options?.babelExclude ?? [];
-  const userModules = options.modulesToTranspile?.map(getModule) ?? [];
-  const modules = [...defaultIncludes, ...userModules];
-  const root = options.projectRoot ?? process.cwd();
-  const userAliases = options.modulesToAlias ?? {};
-  config.module.rules.push({
-    test: /\.([jt]sx?)$/,
-    loader: 'babel-loader',
-    include(filename) {
-      if (!filename) return false;
-      for (const possibleModule of modules) {
-        if (filename.includes(path.normalize(possibleModule))) return true;
-      }
-      if (filename.includes(root)) {
-        for (const excluded of defaultExcludes) {
-          if (filename.includes(path.normalize(excluded))) return false;
-        }
-        return true;
-      }
-      return false;
-    },
-    exclude: babelExclude,
-    options: {
-      root,
-      presets: [
-        [
-          getRnPreset(),
-          {
-            useTransformReactJSXExperimental: true,
-            ...babelPresetReactNativeOptions,
-          },
-        ],
-        [
-          '@babel/preset-react',
-          {
-            runtime: 'automatic',
-            ...babelPresetReactOptions,
-          },
-        ],
-        ...babelPresets,
-      ],
-      plugins: [...babelPlugins],
-    },
-  });
-  config.resolve.extensions = ['.web.js', '.web.jsx', '.web.ts', '.web.tsx', ...config.resolve.extensions];
-  config.resolve.alias = {
-    'react-native$': 'react-native-web',
-    ...config.resolve.alias,
-    ...userAliases,
-  };
-  return config;
-}
+const { reactNativeWebpack } = require('@multiplatform.one/react-native-webpack');
 
 module.exports = {
   mainSrcDir: 'main',
   rendererSrcDir: 'renderer',
   webpack(config) {
     config.module.rules = [];
-    return webpackFinal(config, {
-      babelExclude: [path.resolve(__dirname, 'renderer')],
-      babelPresets: [
-        [
-          require('@babel/preset-env'),
-          {
-            targets: {
-              node: true,
+    config.resolve.extensions = [
+      ...new Set(['.electron.js', '.electron.jsx', '.electron.ts', '.electron.tsx', ...config.resolve.extensions]),
+    ];
+    return reactNativeWebpack(config, {
+      babel: {
+        exclude: [path.resolve(__dirname, 'renderer')],
+        presets: [
+          [
+            require.resolve('@babel/preset-env'),
+            {
+              targets: {
+                node: true,
+              },
             },
-          },
+          ],
+          require.resolve('@babel/preset-typescript'),
         ],
-        require('@babel/preset-typescript'),
-      ],
-      babePlugins: [
-        require('@babel/plugin-transform-class-properties'),
-        [
-          require('@babel/plugin-transform-object-rest-spread'),
-          {
-            useBuiltIns: true,
-          },
+        plugins: [
+          [require.resolve('@babel/plugin-transform-class-properties'), { loose: true }],
+          [
+            require.resolve('@babel/plugin-transform-object-rest-spread'),
+            {
+              useBuiltIns: true,
+            },
+          ],
+          [
+            require.resolve('@babel/plugin-transform-runtime'),
+            {
+              corejs: 3,
+              helpers: true,
+              regenerator: true,
+              useESModules: false,
+            },
+          ],
         ],
-        [
-          require('@babel/plugin-transform-runtime'),
-          {
-            corejs: 3,
-            helpers: true,
-            regenerator: true,
-            useESModules: false,
-          },
-        ],
-      ],
+      },
     });
   },
 };
-
-
-
