9da738c2fa5487331063d73d36487207ea99cd68
tnraju <tnraju1910@gmail.com>
2024-06-18 17:12:17 +0530

    updated RegisterUserProfile with tamagui tag

--- platforms/keycloak/src/login/pages/RegisterUserProfile.tsx
+++ platforms/keycloak/src/login/pages/RegisterUserProfile.tsx
@@ -27,6 +27,7 @@ import { UserProfileFormFields } from './shared/UserProfileFormFields';
 import { clsx } from 'keycloakify/tools/clsx';
 import { useGetClassName } from 'keycloakify/login/lib/useGetClassName';
 import { useState } from 'react';
+import { Anchor, Button, Text, Theme, YStack } from 'ui';
 
 export default function RegisterUserProfile({
   kcContext,
@@ -39,53 +40,84 @@ export default function RegisterUserProfile({
     doUseDefaultCss,
     classes,
   });
+
   const { url, messagesPerField, recaptchaRequired, recaptchaSiteKey } = kcContext;
   const { msg, msgStr } = i18n;
   const [isFormSubmittable, setIsFormSubmittable] = useState(false);
+
   return (
-    <Template
-      {...{ kcContext, i18n, doUseDefaultCss, classes }}
-      displayMessage={messagesPerField.exists('global')}
-      displayRequiredFields={true}
-      headerNode={msg('registerTitle')}
-    >
-      <form id="kc-register-form" className={getClassName('kcFormClass')} action={url.registrationAction} method="post">
-        <UserProfileFormFields
-          kcContext={kcContext}
-          onIsFormSubmittableValueChange={setIsFormSubmittable}
-          i18n={i18n}
-          getClassName={getClassName}
-        />
-        {recaptchaRequired && (
-          <div className="form-group">
-            <div className={getClassName('kcInputWrapperClass')}>
-              <div className="g-recaptcha" data-size="compact" data-sitekey={recaptchaSiteKey} />
-            </div>
-          </div>
-        )}
-        <div className={getClassName('kcFormGroupClass')} style={{ marginBottom: 30 }}>
-          <div id="kc-form-options" className={getClassName('kcFormOptionsClass')}>
-            <div className={getClassName('kcFormOptionsWrapperClass')}>
-              <span>
-                <a href={url.loginUrl}>{msg('backToLogin')}</a>
-              </span>
-            </div>
-          </div>
-          <div id="kc-form-buttons" className={getClassName('kcFormButtonsClass')}>
-            <input
-              className={clsx(
-                getClassName('kcButtonBlockClass'),
-                getClassName('kcButtonClass'),
-                getClassName('kcButtonLargeClass'),
-                getClassName('kcButtonPrimaryClass'),
-              )}
-              type="submit"
-              value={msgStr('doRegister')}
-              disabled={!isFormSubmittable}
-            />
-          </div>
-        </div>
-      </form>
-    </Template>
+    <Theme>
+      <Template
+        {...{ kcContext, i18n, doUseDefaultCss, classes }}
+        displayMessage={messagesPerField.exists('global')}
+        displayRequiredFields={true}
+        headerNode={msg('registerTitle')}
+      >
+        <YStack
+          id="kc-register-form"
+          // className={getClassName('kcFormClass')}
+          // @ts-ignore
+          action={url.registrationAction}
+          method="post"
+        >
+          <UserProfileFormFields
+            kcContext={kcContext}
+            onIsFormSubmittableValueChange={setIsFormSubmittable}
+            i18n={i18n}
+            getClassName={getClassName}
+          />
+          {recaptchaRequired && (
+            <YStack
+            //  className="form-group"
+            >
+              <YStack
+              // className={getClassName('kcInputWrapperClass')}
+              >
+                <YStack
+                  // className="g-recaptcha" data-size="compact"
+                  data-sitekey={recaptchaSiteKey}
+                />
+              </YStack>
+            </YStack>
+          )}
+          <YStack
+          // className={getClassName('kcFormGroupClass')} style={{ marginBottom: 30 }}
+          >
+            <YStack id="kc-form-options" className={getClassName('kcFormOptionsClass')}>
+              <YStack
+              // className={getClassName('kcFormOptionsWrapperClass')}
+              >
+                <Text>
+                  <Anchor color="$backgroundFocus" href={url.loginUrl}>
+                    {msg('backToLogin')}
+                  </Anchor>
+                </Text>
+              </YStack>
+            </YStack>
+            <YStack id="kc-form-buttons" className={getClassName('kcFormButtonsClass')}>
+              {/* <input
+                className={clsx(
+                  getClassName('kcButtonBlockClass'),
+                  getClassName('kcButtonClass'),
+                  getClassName('kcButtonLargeClass'),
+                  getClassName('kcButtonPrimaryClass'),
+                )}
+                type="submit"
+                value={msgStr('doRegister')}
+                disabled={!isFormSubmittable}
+              /> */}
+              <Button
+                // @ts-ignore
+                value={msgStr('doRegister')}
+                disabled={!isFormSubmittable}
+                cursor="pointer"
+              >
+                {msgStr('doRegister')}
+              </Button>
+            </YStack>
+          </YStack>
+        </YStack>
+      </Template>
+    </Theme>
   );
 }
