67ab33b67bf0cc3ee4a0c2d903d2a17afc7ac76a
Dharmendra Boddeda <dharmendra.developer6@gmail.com>
2024-06-19 18:32:15 +0530

    fixed forms

--- packages/components/src/forms/FormRadioGroup/index.tsx
+++ packages/components/src/forms/FormRadioGroup/index.tsx
@@ -1,24 +1,3 @@
-/**
- * File: /src/forms/FormRadioGroup/index.tsx
- * Project: @multiplatform.one/components
- * File Created: 18-10-2023 15:23:17
- * Author: Lalit rajak
- * -----
- * BitSpur (c) Copyright 2021 - 2023
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import React, { useId } from 'react';
 import type { FieldComponentProps, FormControllerProps } from '../types';
 import type { FormFieldProps } from '../FormField';
@@ -41,7 +20,7 @@ export type FormRadioGroupProps<
     fieldProps?: Omit<FormFieldProps, 'helperText' | 'required' | 'error' | 'label'>;
   } & {
     horizontal?: boolean;
-    contentStyle: YStackProps;
+    contentStyle?: YStackProps;
   };
 
 export interface FormRadioProps {
