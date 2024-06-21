78003a4ebc6c543107bbf369510732d69843a36a
Lavanya Katari <katarilavanya881@gmail.com>
2024-06-21 12:06:16 +0530

    working on components export

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,17 +1,71 @@
+/**
+ * File: /src/index.tsx
+ * Project: ui
+ * File Created: 30-05-2024 06:01:12
+ * Author: Clay Risser
+ * -----
+ * BitSpur (c) Copyright 2021 - 2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 export type * from '@multiplatform.one/components';
 export {
+  Accordion,
+  Adapt,
+  AdaptContents,
+  AdaptParentContext,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogOverlay,
+  AlertDialogPortal,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Anchor,
+  Article,
+  Avatar,
+  AvatarFrame,
+  AvatarImage,
   Button,
+  Card,
   Checkbox,
+  FormField,
+  Group,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  Image,
   Input,
+  Label,
+  ListItem,
+  Paragraph,
   Progress,
   RadioGroup,
   Select,
   SelectButton,
+  Sheet,
   Slider,
   Switch,
+  Tabs,
+  Text,
   TextArea,
-  FormField,
-  Toast,
-  AArrowDown,
+  ToggleGroup,
+  XStack,
+  YStack,
 } from '@multiplatform.one/components';
 export * from './tamagui.config';
