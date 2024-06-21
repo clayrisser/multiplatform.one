ed358a5b857b0b823bb3bfc3f0b48de52b49ac51
Dharmendra Boddeda <dharmendra.developer6@gmail.com>
2024-06-21 10:00:29 +0530

    fixed toast export

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,24 +1,3 @@
-/*
- * File: /src/index.ts
- * Project: @multiplatform.one/components
- * File Created: 01-06-2024 13:44:49
- * Author: Clay Risser
- * -----
- * BitSpur (c) Copyright 2021 - 2024
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 export * from './ErrorBoundary';
 export * from './code';
 export * from './forms';
@@ -31,4 +10,5 @@ export * from './organize';
 export * from './panels';
 export * from './table';
 export * from './tints';
-export { Sheet, Toast } from 'tamagui'
+export { Sheet } from 'tamagui';
+export { Toast } from '@tamagui/toast';
