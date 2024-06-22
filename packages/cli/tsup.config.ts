0085098e7dce876590741c48cfbc427db6c7f572
Clay Risser <clayrisser@gmail.com>
2024-06-22 06:02:27 -0500

    Started cli package

---
+++ packages/cli/tsup.config.ts
@@ -0,0 +1,38 @@
+/*
+ * File: /tsup.config.ts
+ * Project: @multiplatform.one/typegraphql
+ * File Created: 04-04-2024 15:50:39
+ * Author: Clay Risser
+ * -----
+ * BitSpur (c) Copyright 2021 - 2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { defineConfig } from 'tsup';
+import { lookupTranspileModules } from '@multiplatform.one/utils/transpileModules';
+
+export default defineConfig({
+  bundle: true,
+  clean: true,
+  dts: true,
+  entry: ['src/**/*.ts?(x)'],
+  entryPoints: ['src/index.ts'],
+  format: ['esm'],
+  minify: false,
+  outDir: 'lib',
+  skipNodeModulesBundle: true,
+  noExternal: lookupTranspileModules([__dirname]),
+  splitting: true,
+  target: 'es2022',
+});
