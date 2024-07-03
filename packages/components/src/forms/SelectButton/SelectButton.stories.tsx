e2185ea770c7b694953da7ff5dff9fd47a92414c
Dharmendra Boddeda <dharmendra.developer6@gmail.com>
2024-07-03 14:43:22 +0530

    fixed expo-storybook

--- packages/components/src/forms/SelectButton/SelectButton.stories.tsx
+++ packages/components/src/forms/SelectButton/SelectButton.stories.tsx
@@ -1,24 +1,3 @@
-/**
- * File: /src/forms/SelectButton/SelectButton.stories.tsx
- * Project: @multiplatform.one/components
- * File Created: 18-06-2024 18:02:12
- * Author: Clay Risser
- * -----
- * BitSpur (c) Copyright 2021 - 2024
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import React from 'react';
 import { SelectButton } from './SelectButton';
 import { Text } from 'tamagui';
@@ -33,15 +12,8 @@ export default {
 };
 
 export const main = (args) => {
-  return <SelectButton {...args}>{args.children}</SelectButton>;
-};
-
-const mainArgs = {
-  value: 'bmw',
-  selectedValue: 'bmw',
-  onValueChange: 'value',
-  children: (
-    <SelectButton onValueChange={action('onValueChange')} gap>
+  return (
+    <SelectButton onValueChange={action('onValueChange')} gap {...args}>
       <SelectButton.OptionButton index={0} value="bmw">
         <Text>BMW</Text>
       </SelectButton.OptionButton>
@@ -55,7 +27,13 @@ const mainArgs = {
         <Text>SUZUKI</Text>
       </SelectButton.OptionButton>
     </SelectButton>
-  ),
+  );
+};
+
+const mainArgs = {
+  value: 'bmw',
+  selectedValue: 'bmw',
+  onValueChange: 'value',
 };
 
 main.args = mainArgs;
