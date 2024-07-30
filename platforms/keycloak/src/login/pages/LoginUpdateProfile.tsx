9f1bfa774331f816088ec1ad170c4900d3cc497c
tnaju <tnraju1910@gmail.com>
2024-07-30 18:20:45 +0530

    updated login screen theme

--- platforms/keycloak/src/login/pages/LoginUpdateProfile.tsx
+++ platforms/keycloak/src/login/pages/LoginUpdateProfile.tsx
@@ -3,12 +3,22 @@ import type { PageProps } from 'keycloakify/login/pages/PageProps';
 import { useGetClassName } from 'keycloakify/login/lib/useGetClassName';
 import type { KcContext } from '../kcContext';
 import type { I18n } from '../i18n';
+import { FieldInput, Paragraph, SubmitButton, YStack } from 'ui';
+import { useForm } from '@tanstack/react-form';
+import { useRef, useState } from 'react';
+
+export interface FormError {
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  username?: string;
+}
 
 export default function LoginUpdateProfile(
   props: PageProps<Extract<KcContext, { pageId: 'login-update-profile.ftl' }>, I18n>,
 ) {
   const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
-
+  const formRef = useRef<HTMLFormElement | null>(null);
   const { getClassName } = useGetClassName({
     doUseDefaultCss,
     classes,
@@ -18,34 +28,117 @@ export default function LoginUpdateProfile(
 
   const { url, user, messagesPerField, isAppInitiatedAction } = kcContext;
 
+  console.log('user', user);
+  console.log('kc context--->', kcContext);
+
+  const mainFields = ['firstName', 'lastName', 'email', 'username'];
+  // @ts-ignore
+  const extraFields = Object.keys(kcContext?.profile?.attributesByName || {}).filter(
+    (field) => !mainFields.includes(field),
+  );
+  const [error, setError] = useState<FormError>({});
+  const form = useForm({
+    defaultValues: {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      username: user.username,
+      ...Object.fromEntries(extraFields.map((field) => [field, ''])),
+    },
+    onSubmit: ({ value }) => {
+      console.log('form submited');
+      let flag = false;
+      setError({});
+
+      Object.entries(value).forEach(([name, value]) => {
+        if (name === 'firstName' || name === 'lastName' || name === 'email' || name === 'userName') {
+          if (!value || value === '') {
+            flag = true;
+            setError((prev) => ({ ...prev, [name]: `${name} is required` }));
+          }
+        }
+        if (flag) return;
+        if (!value) return;
+
+        const input = document.createElement('input');
+        input.name = name;
+        typeof value === 'boolean' ? (input.checked = value) : (input.value = value || '');
+        input.type = 'hidden';
+        input.style.display = 'none';
+        formRef.current?.appendChild(input);
+      });
+      formRef.current?.submit();
+    },
+  });
+
+  const handleChangeFirstName = (value: string) => {
+    if (!value || value === '') {
+      return setError((prev) => ({ ...prev, firstName: 'first name is required' }));
+    }
+    setError((prev) => ({ ...prev, firstName: '' }));
+  };
+
+  const handleChangeLastName = (value: string) => {
+    if (!value || value === '') {
+      return setError((prev) => ({ ...prev, lastName: 'last name is required' }));
+    }
+    setError((prev) => ({ ...prev, lastName: '' }));
+  };
+
+  const handleChangeEmail = (value: string) => {
+    if (!value || value === '') {
+      return setError((prev) => ({ ...prev, email: 'email is required' }));
+    }
+    setError((prev) => ({ ...prev, email: '' }));
+  };
+
+  const handleChangeUserName = (value: string) => {
+    if (!value || value === '') {
+      return setError((prev) => ({ ...prev, username: 'user name is required' }));
+    }
+    setError((prev) => ({ ...prev, username: '' }));
+  };
+
   return (
     <Template {...{ kcContext, i18n, doUseDefaultCss, classes }} headerNode={msg('loginProfileTitle')}>
-      <form id="kc-update-profile-form" className={getClassName('kcFormClass')} action={url.loginAction} method="post">
+      <form ref={formRef} id="kc-update-profile-form" action={url.loginAction} method="post">
         {user.editUsernameAllowed && (
-          <div
-            className={clsx(
-              getClassName('kcFormGroupClass'),
-              messagesPerField.printIfExists('username', getClassName('kcFormGroupErrorClass')),
-            )}
-          >
-            <div className={getClassName('kcLabelWrapperClass')}>
-              <label htmlFor="username" className={getClassName('kcLabelClass')}>
-                {msg('username')}
-              </label>
-            </div>
-            <div className={getClassName('kcInputWrapperClass')}>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                defaultValue={user.username ?? ''}
-                className={getClassName('kcInputClass')}
-              />
-            </div>
-          </div>
+          // <div
+          //   className={clsx(
+          //     getClassName('kcFormGroupClass'),
+          //     messagesPerField.printIfExists('username', getClassName('kcFormGroupErrorClass')),
+          //   )}
+          // >
+          //   <div className={getClassName('kcLabelWrapperClass')}>
+          //     <label htmlFor="username" className={getClassName('kcLabelClass')}>
+          //       {msg('username')}
+          //     </label>
+          //   </div>
+          //   <div className={getClassName('kcInputWrapperClass')}>
+          //     <input
+          //       type="text"
+          //       id="username"
+          //       name="username"
+          //       defaultValue={user.username ?? ''}
+          //       className={getClassName('kcInputClass')}
+          //     />
+          //   </div>
+          // </div>
+          <FieldInput
+            form={form}
+            id="username"
+            name="username"
+            label={msg('username')}
+            // @ts-ignore
+            type="text"
+            onChangeText={handleChangeUserName}
+            required
+            defaultValue={user.username ?? ''}
+          />
         )}
+        {error.username && <Paragraph color="$red9">{error.username}</Paragraph>}
 
-        <div
+        {/* <div
           className={clsx(
             getClassName('kcFormGroupClass'),
             messagesPerField.printIfExists('email', getClassName('kcFormGroupErrorClass')),
@@ -65,9 +158,20 @@ export default function LoginUpdateProfile(
               className={getClassName('kcInputClass')}
             />
           </div>
-        </div>
-
-        <div
+        </div> */}
+        <FieldInput
+          form={form}
+          label={msg('email')}
+          id="email"
+          name="email"
+          onChangeText={handleChangeEmail}
+          required
+          // @ts-ignore
+          type="text"
+          defaultValue={user.email ?? ''}
+        />
+        {error.email && <Paragraph color="$red9">{error.email}</Paragraph>}
+        {/* <div
           className={clsx(
             getClassName('kcFormGroupClass'),
             messagesPerField.printIfExists('firstName', getClassName('kcFormGroupErrorClass')),
@@ -87,9 +191,21 @@ export default function LoginUpdateProfile(
               className={getClassName('kcInputClass')}
             />
           </div>
-        </div>
+        </div> */}
 
-        <div
+        <FieldInput
+          form={form}
+          label={msg('firstName')}
+          id="firstName"
+          name="firstName"
+          onChangeText={handleChangeFirstName}
+          required
+          // @ts-ignore
+          type="text"
+          defaultValue={user.firstName ?? ''}
+        />
+        {error.firstName && <Paragraph color="$red9">{error.username}</Paragraph>}
+        {/* <div
           className={clsx(
             getClassName('kcFormGroupClass'),
             messagesPerField.printIfExists('lastName', getClassName('kcFormGroupErrorClass')),
@@ -109,17 +225,55 @@ export default function LoginUpdateProfile(
               className={getClassName('kcInputClass')}
             />
           </div>
-        </div>
+        </div> */}
 
-        <div className={getClassName('kcFormGroupClass')}>
-          <div id="kc-form-options" className={getClassName('kcFormOptionsClass')}>
-            <div className={getClassName('kcFormOptionsWrapperClass')} />
-          </div>
+        <FieldInput
+          form={form}
+          label={msg('lastName')}
+          id="lastName"
+          name="lastName"
+          onChangeText={handleChangeLastName}
+          required
+          // @ts-ignore
+          type="text"
+          defaultValue={user.lastName ?? ''}
+        />
+        {error.lastName && <Paragraph color="$red9">{error.lastName}</Paragraph>}
+        {extraFields.map((field, index) => {
+          return (
+            <FieldInput
+              key={field}
+              form={form}
+              id={field}
+              label={field}
+              // @ts-ignore
+              name={field}
+              tabIndex={mainFields.length + 2 + (index + 1)}
+              inputProps={{
+                autoComplete: 'off',
+              }}
+            />
+          );
+        })}
+        <YStack
+        // className={getClassName('kcFormGroupClass')}
+        >
+          <YStack
+            id="kc-form-options"
+            // className={getClassName('kcFormOptionsClass')}
+          >
+            <YStack
+            // className={getClassName('kcFormOptionsWrapperClass')}
+            />
+          </YStack>
 
-          <div id="kc-form-buttons" className={getClassName('kcFormButtonsClass')}>
+          <YStack
+            id="kc-form-buttons"
+            // className={getClassName('kcFormButtonsClass')}
+          >
             {isAppInitiatedAction ? (
               <>
-                <input
+                {/* <input
                   className={clsx(
                     getClassName('kcButtonClass'),
                     getClassName('kcButtonPrimaryClass'),
@@ -127,8 +281,9 @@ export default function LoginUpdateProfile(
                   )}
                   type="submit"
                   defaultValue={msgStr('doSubmit')}
-                />
-                <button
+                /> */}
+                <SubmitButton form={form}>{msgStr('doSubmit')}</SubmitButton>
+                {/* <button
                   className={clsx(
                     getClassName('kcButtonClass'),
                     getClassName('kcButtonDefaultClass'),
@@ -139,22 +294,34 @@ export default function LoginUpdateProfile(
                   value="true"
                 >
                   {msg('doCancel')}
-                </button>
+                </button> */}
+                <SubmitButton
+                  form={form}
+                  // @ts-ignore
+                  type="submit"
+                  name="cancel-aia"
+                  value="true"
+                >
+                  {msg('doCancel')}
+                </SubmitButton>
               </>
             ) : (
-              <input
-                className={clsx(
-                  getClassName('kcButtonClass'),
-                  getClassName('kcButtonPrimaryClass'),
-                  getClassName('kcButtonBlockClass'),
-                  getClassName('kcButtonLargeClass'),
-                )}
-                type="submit"
-                defaultValue={msgStr('doSubmit')}
-              />
+              // <input
+              //   className={clsx(
+              //     getClassName('kcButtonClass'),
+              //     getClassName('kcButtonPrimaryClass'),
+              //     getClassName('kcButtonBlockClass'),
+              //     getClassName('kcButtonLargeClass'),
+              //   )}
+              //   type="submit"
+              //   defaultValue={msgStr('doSubmit')}
+              // />
+              <SubmitButton form={form} bg="$backgroundFocus">
+                {msgStr('doSubmit')}
+              </SubmitButton>
             )}
-          </div>
-        </div>
+          </YStack>
+        </YStack>
       </form>
     </Template>
   );
