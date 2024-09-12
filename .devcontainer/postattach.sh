43b3a5986dcebd2b9c5485dd565f9ffb94f56ccc
Clay Risser <clayrisser@gmail.com>
2024-09-12 06:25:54 -0400

    Fixed pnpm

--- .devcontainer/postattach.sh
+++ .devcontainer/postattach.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+pnpm config set store-dir /home/frappe/.pnpm-store
+sudo chown frappe:frappe /home/frappe/.pnpm-store
 git config --global --add safe.directory "$(pwd)"
+git config --global --add safe.directory "$(pwd)/frappe/frappe-bench/apps/frappe_graphql"
+git config --global --add safe.directory "$(pwd)/frappe/frappe-bench/apps/frappe_keycloak"
 make frappe/install
 make build
