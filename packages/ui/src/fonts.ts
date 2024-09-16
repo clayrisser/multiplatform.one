2c9e33134b79cc95aff91380893eb3f4d0671d56
Clay Risser <clayrisser@gmail.com>
2024-09-16 11:21:34 +0000

    Fixed fonts type error

--- packages/ui/src/fonts.ts
+++ packages/ui/src/fonts.ts
@@ -1,14 +1,46 @@
+/*
+ * File: /src/fonts.ts
+ * Project: ui
+ * File Created: 30-05-2024 10:01:12
+ * Author: Clay Risser
+ * -----
+ * BitSpur (c) Copyright 2021 - 2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { createInterFont } from "@tamagui/font-inter";
 import { createDefaultFont } from "multiplatform.one";
+import type { GenericFonts } from "@tamagui/web/src/types";
 
-export interface Fonts {
-  body: ReturnType<typeof createInterFont>;
-  heading: ReturnType<typeof createInterFont>;
-  rockSalt: ReturnType<typeof createDefaultFont>;
-  silkscreen: ReturnType<typeof createDefaultFont>;
-}
+// export interface Fonts {
+//   body: ReturnType<typeof createInterFont>;
+//   heading: ReturnType<typeof createInterFont>;
+//   rockSalt: ReturnType<typeof createDefaultFont>;
+//   silkscreen: ReturnType<typeof createDefaultFont>;
+// }
 
-export const fonts: Fonts = {
+type OmitLanguagePostfix<Set> = Set extends string
+  ? Set extends `${infer Prefix}_${string}`
+    ? Prefix
+    : Set
+  : never;
+
+type RemoveLanguagePostfixes<F extends GenericFonts> = {
+  [Key in OmitLanguagePostfix<keyof F>]: F[Key];
+};
+
+export const fonts: RemoveLanguagePostfixes<GenericFonts> = {
   rockSalt: createDefaultFont({
     family: "Rock Salt",
   }),
