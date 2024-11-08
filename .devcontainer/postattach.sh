9afeba76fc025d258600fa8147155e3bb3a7649e
Clay Risser <clayrisser@gmail.com>
2024-11-08 06:42:27 +0000

    Improved frappe bootstrap

--- .devcontainer/postattach.sh
+++ .devcontainer/postattach.sh
@@ -6,5 +6,5 @@ sudo chown frappe:frappe /home/frappe/.pnpm-store
 git config --global --add safe.directory "$(pwd)"
 git config --global --add safe.directory "$(pwd)/frappe/frappe-bench/apps/frappe_graphql"
 git config --global --add safe.directory "$(pwd)/frappe/frappe-bench/apps/frappe_keycloak"
-make frappe/install
+make frappe/bootstrap
 make build
